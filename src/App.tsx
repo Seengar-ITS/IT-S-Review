f69de0d731f621eb7a38a0695d6624b2f6150a1f
Seengar <seengar237@gmail.com>
2026-05-13 15:27:29 +0500

    fix: correct invalid import identifier My-reviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
-import My-reviews from './pages/My-reviews'
+import MyReviews from './pages/My-reviews'
 import Search from './pages/Search'
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/my-reviews" element={<My-reviews />} />
+          <Route path="/my-reviews" element={<MyReviews />} />
           <Route path="/search" element={<Search />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
